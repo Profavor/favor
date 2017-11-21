5fa6b29e82a711dcf2ab39af3595c94905418414
profavor <profavor@naver.com>
2017-11-21 21:54:50 +0900

    First commit

---
+++ src/main/java/com/favor/app/FavorApplication.java
@@ -0,0 +1,12 @@
+package com.favor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavorApplication.class, args);
+	}
+}
